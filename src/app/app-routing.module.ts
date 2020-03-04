b81f51b6eb0e210cd91dd6ccbfa981f8913afe24
anazucchelli@gmail.com <120818analu>
2020-03-04 17:35:31 -0300

    dias dois com angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ContatoComponent } from './components/contato/contato.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent
+  },
+  {
+    path:"sobre",
+    component:SobreComponent
+  },
+  {
+    path:"contato",
+    component:ContatoComponent
+  },
+  {
+    path:"home",
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/grupos/grupos.component.ts
@@ -0,0 +1,23 @@
+import { Component, Output,EventEmitter } from '@angular/core';
+import { Grupo } from "src/app/models/Grupo";
+import { HttpService } from 'src/app/services/http.service';
+
+@Component({
+  selector: 'app-grupos',
+  templateUrl: './grupos.component.html',
+  styleUrls: ['./grupos.component.css']
+})
+export class GruposComponent {
+
+  public grupos: Grupo[] = [];
+  @Output() grupoClicado = new EventEmitter();
+
+  constructor(private http:HttpService) { 
+
+    this.http.getGrupos().subscribe(
+      (data) => {this.grupos = data}
+    );
+  }
+  
+  
+}
